5c6f225ba0c437e89343fefa03219f6e893468ed
Hafsa-Begum <hafsagood@gmail.com>
2021-10-31 23:51:10 +0600

    'finalllll'

--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -27,7 +27,7 @@ const Reviews = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4 col-sm-6 col-12 rounded-3 mb-3">
+                <div className="col-md-4 col-sm-6 col-12 mb-3">
                     <div className='row px-3 pb-3 pt-4 bg-white rounded-3 mx-3'>
                         <div className="col-md-4">
                             <img className='w-100 rounded-circle' src="https://htmldesigntemplates.com/html/yatra/bootstrap4/images/testemonial1.jpg" alt="" />
